9b37c363b467f95fa353a22dffa952c7408fe699
zenntma <zenntma@gmail.com>
2025-06-23 12:16:07 +0700

    homde detail

---
+++ lib/home.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Map<String, String>> recipes = [
+      {
+        'title': 'Nasi Goreng',
+        'image':
+            'https://img-global.cpcdn.com/recipes/72060050b3fccc52/1200x630cq70/photo.jpg'
+      },
+      {
+        'title': 'Mie Goreng',
+        'image':
+            'https://img-global.cpcdn.com/recipes/8a606063d0c0e547/1200x630cq70/photo.jpg'
+      },
+      {
+        'title': 'Donat Kentang',
+        'image':
+            'https://img-global.cpcdn.com/recipes/ba468e2e4040d3ba/1200x630cq70/photo.jpg'
+      },
+      {
+        'title': 'Es Teler',
+        'image':
+            'https://img-global.cpcdn.com/recipes/1a962f8d833b0dff/1200x630cq70/photo.jpg'
+      },
+    ];
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFF5F0E7),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFF5F0E7),
+        elevation: 0,
+        title: const Text(
+          'ResepKu',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://cdn-icons-png.flaticon.com/512/4140/4140048.png'),
+            ),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // Search Bar
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Cari resep...',
+                prefixIcon: const Icon(Icons.search),
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding: const EdgeInsets.symmetric(vertical: 0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            // GridView Resep
+            Expanded(
+              child: GridView.builder(
+                itemCount: recipes.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 12,
+                  crossAxisSpacing: 12,
+                  childAspectRatio: 0.8,
+                ),
+                itemBuilder: (context, index) {
+                  final recipe = recipes[index];
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        ClipRRect(
+                          borderRadius: const BorderRadius.vertical(
+                              top: Radius.circular(12)),
+                          child: Image.network(
+                            recipe['image']!,
+                            height: 100,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            recipe['title']!,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      // Bottom Navigation
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color(0xFFE8E1D5),
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black54,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_box_outlined),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border),
+            label: '',
+          ),
+        ],
+      ),
+    );
+  }
+}
