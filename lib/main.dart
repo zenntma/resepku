81b7030ba81cd6eaead8d8f4d30c26d109c3eaf4
zenntma <zenntma@gmail.com>
2025-06-23 12:45:39 +0700

    home

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:resepku/splashscreen.dart';
+import 'package:resepku/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: SplashScreen(),
+      home: HomePage(),
     );
   }
 }
