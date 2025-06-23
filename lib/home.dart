1153fb2ce0cb3639584f9dce7cbc182efa9e552d
tugas45 <racerftail@gmail.com>
2025-06-23 12:37:58 +0700

    update home detail

--- lib/home.dart
+++ lib/home.dart
@@ -9,22 +9,22 @@ class HomePage extends StatelessWidget {
       {
         'title': 'Nasi Goreng',
         'image':
-            'https://img-global.cpcdn.com/recipes/72060050b3fccc52/1200x630cq70/photo.jpg'
+            'https://img-global.cpcdn.com/recipes/72060050b3fccc52/1200x630cq70/photo.jpg',
       },
       {
         'title': 'Mie Goreng',
         'image':
-            'https://img-global.cpcdn.com/recipes/8a606063d0c0e547/1200x630cq70/photo.jpg'
+            'https://img-global.cpcdn.com/recipes/8a606063d0c0e547/1200x630cq70/photo.jpg',
       },
       {
         'title': 'Donat Kentang',
         'image':
-            'https://img-global.cpcdn.com/recipes/ba468e2e4040d3ba/1200x630cq70/photo.jpg'
+            'https://img-global.cpcdn.com/recipes/ba468e2e4040d3ba/1200x630cq70/photo.jpg',
       },
       {
         'title': 'Es Teler',
         'image':
-            'https://img-global.cpcdn.com/recipes/1a962f8d833b0dff/1200x630cq70/photo.jpg'
+            'https://img-global.cpcdn.com/recipes/1a962f8d833b0dff/1200x630cq70/photo.jpg',
       },
     ];
 
@@ -35,86 +35,97 @@ class HomePage extends StatelessWidget {
         elevation: 0,
         title: const Text(
           'ResepKu',
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-          ),
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         ),
         actions: [
           Padding(
             padding: const EdgeInsets.only(right: 16),
             child: CircleAvatar(
               backgroundImage: NetworkImage(
-                  'https://cdn-icons-png.flaticon.com/512/4140/4140048.png'),
+                'https://cdn-icons-png.flaticon.com/512/4140/4140048.png',
+              ),
+              radius: 20,
             ),
-          )
+          ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            // Search Bar
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Cari resep...',
-                prefixIcon: const Icon(Icons.search),
-                filled: true,
-                fillColor: Colors.white,
-                contentPadding: const EdgeInsets.symmetric(vertical: 0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide.none,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              // Search Bar
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Cari resep...',
+                  prefixIcon: const Icon(Icons.search),
+                  filled: true,
+                  fillColor: Colors.white,
+                  contentPadding: const EdgeInsets.symmetric(vertical: 0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide.none,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            // GridView Resep
-            Expanded(
-              child: GridView.builder(
-                itemCount: recipes.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 12,
-                  crossAxisSpacing: 12,
-                  childAspectRatio: 0.8,
-                ),
-                itemBuilder: (context, index) {
-                  final recipe = recipes[index];
-                  return Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        ClipRRect(
-                          borderRadius: const BorderRadius.vertical(
-                              top: Radius.circular(12)),
-                          child: Image.network(
-                            recipe['image']!,
-                            height: 100,
-                            fit: BoxFit.cover,
+              const SizedBox(height: 20),
+              // GridView Resep
+              Expanded(
+                child: GridView.builder(
+                  itemCount: recipes.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16,
+                    crossAxisSpacing: 16,
+                    childAspectRatio: 0.8,
+                  ),
+                  itemBuilder: (context, index) {
+                    final recipe = recipes[index];
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 6,
+                            offset: Offset(0, 3),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            recipe['title']!,
-                            textAlign: TextAlign.center,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
+                        ],
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          ClipRRect(
+                            borderRadius: const BorderRadius.vertical(
+                              top: Radius.circular(12),
+                            ),
+                            child: Image.network(
+                              recipe['image']!,
+                              height: 100,
+                              fit: BoxFit.cover,
                             ),
                           ),
-                        )
-                      ],
-                    ),
-                  );
-                },
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              recipe['title']!,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
       // Bottom Navigation
@@ -123,18 +134,12 @@ class HomePage extends StatelessWidget {
         selectedItemColor: Colors.black,
         unselectedItemColor: Colors.black54,
         items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: '',
-          ),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
           BottomNavigationBarItem(
             icon: Icon(Icons.add_box_outlined),
             label: '',
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite_border),
-            label: '',
-          ),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite_border), label: ''),
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:resepku/splashscreen.dart';
+import 'package:resepku/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ResepKu',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: SplashScreen(),
+      home: HomePage(),
     );
   }
 }
